d357688ef4c7701ca67e58fcad9cc2668931dcbf
Hm <Manjunath_Hm@Dellteam.com>
2020-05-25 17:09:22 +0530

    Added Eureka Server module

---
+++ discovery-server/src/test/java/org/fourstack/microservices/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fourstack.microservices.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
