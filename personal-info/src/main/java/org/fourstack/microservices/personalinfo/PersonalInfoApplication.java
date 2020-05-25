2296d4af6ddf38e5577fdd667ce3969d8942f4bd
Hm <Manjunath_Hm@Dellteam.com>
2020-05-25 17:42:30 +0530

    Added Eureka Client Dependencies and Added @EnableEurekaClient(Optional Annotation)

--- personal-info/src/main/java/org/fourstack/microservices/personalinfo/PersonalInfoApplication.java
+++ personal-info/src/main/java/org/fourstack/microservices/personalinfo/PersonalInfoApplication.java
@@ -2,8 +2,10 @@ package org.fourstack.microservices.personalinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PersonalInfoApplication {
 
 	public static void main(String[] args) {
